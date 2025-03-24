dc8976daa40f97e1b9f1f319013ead04d214e364
Kuol Deng <kuoldimdeng@outlook.com>
2025-03-24 17:40:15 -0600

    Real Progress

--- frontend/pyfactor_next/src/app/dashboard/components/AppBar.jsx
+++ frontend/pyfactor_next/src/app/dashboard/components/AppBar.jsx
@@ -158,7 +158,7 @@ const AppBar = ({
             src={logoPath}
             alt="PyFactor Logo"
             width={100}
-            height={40}
+            height={90}
             style={{ marginLeft: '-10px', objectFit: 'contain' }}
           />
         </Box>
