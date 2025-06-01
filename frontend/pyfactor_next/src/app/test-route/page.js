8d12ea51507a7807b1cec188b9ebaea5856a54d7
Kuol Deng <kuoldimdeng@outlook.com>
2025-06-01 12:41:23 -0600

    Fix: Disable problematic middleware causing 500 errors - auth0.middleware doesn't exist in v4.6.0

---
+++ frontend/pyfactor_next/src/app/test-route/page.js
@@ -0,0 +1,8 @@
+export default function TestRoute() {
+  return (
+    <div style={{ padding: '50px', textAlign: 'center' }}>
+      <h1>Test Route Working!</h1>
+      <p>If you can see this, the routing is working correctly.</p>
+    </div>
+  );
+} 
