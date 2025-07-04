9dcd31bbca6b1177a55eb286e6cf478e9f3b7ef7
Kuol Deng <kuoldimdeng@outlook.com>
2025-07-04 16:38:10 -0600

    Fix migration dependency reference
    - Update 0011_merge_20250626_1549 to reference renamed migration 0014_set_first_user_as_owner
    - This fixes 'Migration dependencies reference nonexistent parent node' error

--- backend/pyfactor/custom_auth/migrations/0011_merge_20250626_1549.py
+++ backend/pyfactor/custom_auth/migrations/0011_merge_20250626_1549.py
@@ -6,7 +6,7 @@ from django.db import migrations
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('custom_auth', '0004_set_first_user_as_owner'),
+        ('custom_auth', '0014_set_first_user_as_owner'),
         ('custom_auth', '0010_add_subscription_plan_to_user'),
     ]
 
