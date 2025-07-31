67328a05b7df8f8ae200ab1247b115e49f6ffd79
Kuol Deng <kuoldimdeng@outlook.com>
2025-07-31 15:50:18 -0600

    fix: Move cache decorators to correct location in core/decorators directory

--- backend/pyfactor/core/decorators/__init__.py
+++ backend/pyfactor/core/decorators/__init__.py
@@ -1,3 +1,4 @@
 from .deprecation import deprecated_endpoint, deprecated_class_view
+from .cache_decorators import cache_view, invalidate_on_change, cache_page_view
 
-__all__ = ['deprecated_endpoint', 'deprecated_class_view']
+__all__ = ['deprecated_endpoint', 'deprecated_class_view', 'cache_view', 'invalidate_on_change', 'cache_page_view']
