8df04037cf239cf1f15bd03d8b7830d6683fd2ee
Kuol Deng <kuoldimdeng@outlook.com>
2025-09-09 16:37:53 -0600

    Remove replaces attribute from courier migration causing KeyError
    The replaces attribute was causing Django to look for a non-existent migration
    in the graph, resulting in KeyError: ('couriers', '0001_initial')
    
    This fix allows the migration to run without circular references.

--- backend/pyfactor/couriers/migrations/0001_initial.py
+++ backend/pyfactor/couriers/migrations/0001_initial.py
@@ -52,8 +52,6 @@ def reverse_courier_tables(apps, schema_editor):
 class Migration(migrations.Migration):
 
     initial = True
-    
-    replaces = [('couriers', '0001_initial')]
 
     dependencies = [
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
